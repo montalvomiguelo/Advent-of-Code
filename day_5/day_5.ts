c6ffa941b6d48727bc918e024a66157b2e87d663
Miguel Montalvo <miguel.montalvo@barrelny.com>
2022-12-08 21:36:42 -0800

    Day 5 part 1

---
+++ day_5/day_5.ts
@@ -0,0 +1,54 @@
+export function message(input: string): string {
+  const inputArray = input.split("\n\n");
+  const rows = inputArray[0].split("\n").filter(Boolean);
+  const lastRow = rows[rows.length - 1];
+  const stacks: Record<string, string[]> = {};
+  const procedures = inputArray[1].trim().split("\n");
+
+  const columnIndexes: number[] = [];
+
+  for (let i = 0; i < lastRow.length; i++) {
+    if (lastRow[i]?.match(/\d/)) {
+      columnIndexes.push(i);
+    }
+  }
+
+  for (let i = 0; i < rows.length - 1; i++) {
+    const row = rows[i];
+    columnIndexes.forEach((index) => {
+      const value = row[index];
+      const key = index.toString();
+      if (!stacks[key]) {
+        stacks[key] = [];
+      }
+      if (value !== " ") {
+        stacks[key].unshift(value);
+      }
+    });
+  }
+
+  procedures.forEach((procedure) => {
+    const procedureFragments = procedure.split(" ");
+    const proceduresCount = +procedureFragments[1];
+    const from = +procedureFragments[3];
+    const to = +procedureFragments[5];
+    const stackKeys = Object.keys(stacks);
+    const fromKey = stackKeys[from - 1];
+    const toKey = stackKeys[to - 1];
+    const fromStack = stacks[fromKey];
+    const toStack = stacks[toKey];
+
+    for (let i = 0; i < proceduresCount; i++) {
+      const value = fromStack.pop() as string;
+      toStack.push(value);
+    }
+  });
+
+  return Object.values(stacks).reduce(
+    (result, stack) => result + stack[stack.length - 1],
+    "",
+  );
+}
+
+const input = await Deno.readTextFile("./input.txt");
+console.log(message(input));
---
+++ day_5/day_5_test.ts
@@ -0,0 +1,19 @@
+import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+import { message } from "./day_5.ts";
+
+Deno.test("Day 5: Supply Stacks", () => {
+  const input = `
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+  `;
+
+  // Part 1
+  assertEquals(message(input), "CMZ");
+});
