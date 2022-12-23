214fc6c40d2833d0a37b22acb8125c08745272a3
Miguel Montalvo <miguel.montalvo@barrelny.com>
2022-12-23 12:31:33 -0600

    Day 11 part 1

---
+++ day_11/day_11.ts
@@ -0,0 +1,54 @@
+export function result(input: string): number {
+  const data = input.trim().split("\n\n").map((item) => item.split("\n"));
+  const worryLevels: Record<string, number[]> = {};
+  const inspectionsCount: Record<string, number> = {};
+
+  for (let i = 0; i < data.length; i++) {
+    const rows = data[i];
+    const match = rows[1].match(/\d.*/) as RegExpMatchArray;
+    const items = match[0].split(",").map((item) => parseInt(item));
+
+    worryLevels[i] = items;
+  }
+
+  for (let i = 0; i < 20; i++) {
+    for (let j = 0; j < data.length; j++) {
+      const rows = data[j];
+      const operationRow = rows[2].trim();
+      const operator = operationRow.split(" ")[4];
+      const operationNumber = operationRow.split(" ")[5];
+      const testNumber = parseInt(rows[3].split(" ")[5]);
+      inspectionsCount[j] = inspectionsCount[j]
+        ? inspectionsCount[j] + worryLevels[j].length
+        : worryLevels[j].length;
+
+      while (worryLevels[j].length) {
+        const item = worryLevels[j][0];
+
+        const operation = operator === "*"
+          ? operationNumber === "old"
+            ? item * item
+            : item * parseInt(operationNumber)
+          : operationNumber === "old"
+          ? item + item
+          : item + parseInt(operationNumber);
+
+        const result = Math.floor(operation / 3);
+        const test = result % testNumber === 0;
+
+        const nextMonkeyRow = test ? rows[4] : rows[5];
+        const nextMonkeyNumber = nextMonkeyRow.split(" ")[9];
+
+        worryLevels[j].splice(0, 1);
+        worryLevels[nextMonkeyNumber].push(result);
+      }
+    }
+  }
+
+  const inspections = Object.values(inspectionsCount).sort((a, b) => b -a).slice(0, 2);
+
+  return inspections[0] * inspections[1];
+}
+
+const input = await Deno.readTextFile("./input.txt");
+console.log(result(input));
---
+++ day_11/day_11_test.ts
@@ -0,0 +1,36 @@
+import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+import { result } from "./day_11.ts";
+
+Deno.test("Day 11: Monkey in the Middle", () => {
+  const input = `
+Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1
+  `;
+  // Part 1
+  assertEquals(result(input), 10605);
+});
