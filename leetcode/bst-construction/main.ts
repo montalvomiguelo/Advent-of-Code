897d13e8582d0298b7edbb3b83dd7ddf9c87d91c
Miguel Montalvo <miguel.montalvo@barrelny.com>
2023-02-05 19:08:38 -0800

    ✨ BST Construction

---
+++ leetcode/bst-construction/main.ts
@@ -0,0 +1,152 @@
+export class BST {
+  value: number;
+  left: BST | null;
+  right: BST | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value: number, self: BST = this): BST {
+    const newTree = new BST(value);
+    let tree: BST | null = self;
+    let prevTree: BST | null = null;
+
+    while (tree) {
+      prevTree = tree;
+
+      if (value < tree.value) {
+        tree = tree.left;
+
+        if (!tree) {
+          prevTree.left = newTree;
+        }
+      } else {
+        tree = tree.right;
+
+        if (!tree) {
+          prevTree.right = newTree;
+        }
+      }
+    }
+
+    return this;
+  }
+
+  contains(value: number, self: BST = this): boolean {
+    let tree: BST | null = self;
+
+    while (tree) {
+      if (value === tree.value) {
+        return true;
+      }
+
+      if (value < tree.value) {
+        tree = tree.left;
+      } else {
+        tree = tree.right;
+      }
+    }
+
+    return false;
+  }
+
+  remove(value: number, self: BST = this): BST {
+    let tree: BST | null = self;
+    let prevTree: BST | null = self;
+
+    while (tree) {
+      if (value === tree.value) {
+        if (!tree.left && !tree.right) {
+          if (tree.value < prevTree.value) {
+            prevTree.left = null;
+          } else {
+            prevTree.right = null;
+          }
+        }
+        if (!tree.left && tree.right) {
+          if (prevTree === tree) {
+            tree.value = tree.right.value;
+            tree.left = tree.right.left;
+            tree.right = tree.right.right;
+          } else {
+            if (tree.value < prevTree.value) {
+              prevTree.left = tree.right;
+            } else {
+              prevTree.right = tree.right;
+            }
+          }
+        } else if (!tree.right && tree.left) {
+          if (prevTree === tree) {
+            tree.value = tree.left.value;
+            tree.right = tree.left.right;
+            tree.left = tree.left.left;
+          } else {
+            if (tree.value < prevTree.value) {
+              prevTree.left = tree.left;
+            } else {
+              prevTree.right = tree.left;
+            }
+          }
+        } else {
+          const [successor, prevSuccessor] = this.inOrderSuccessor(tree);
+
+          if (successor) {
+            if (tree.value < prevTree.value) {
+              if (prevTree.left) {
+                prevTree.left.value = successor.value;
+              }
+            } else if (prevTree === tree) {
+              tree.value = successor.value;
+            } else {
+              if (prevTree.right) {
+                prevTree.right.value = successor.value;
+              }
+            }
+
+            if (prevSuccessor) {
+              if (successor.value < prevSuccessor.value) {
+                prevSuccessor.left = null;
+              } else {
+                prevSuccessor.right = successor.right;
+              }
+            }
+          }
+        }
+        break;
+      }
+
+      if (value < tree.value) {
+        prevTree = tree;
+        tree = tree.left;
+      } else {
+        prevTree = tree;
+        tree = tree.right;
+      }
+    }
+
+    return this;
+  }
+
+  inOrderSuccessor(tree: BST): Array<BST | null> {
+    let successor: BST | null = tree.right;
+    let prevSuccessor = tree;
+
+    while (successor) {
+      if (successor.left) {
+        prevSuccessor = successor;
+        successor = successor.left;
+
+        if (!successor.left && !successor.right) {
+          break;
+        }
+      } else {
+        break;
+      }
+    }
+
+    return [successor, prevSuccessor];
+  }
+}
---
+++ leetcode/bst-construction/main_test.ts
@@ -0,0 +1,133 @@
+import {
+  assertEquals,
+  assertExists,
+} from "https://deno.land/std@0.167.0/testing/asserts.ts";
+import { BST } from "./main.ts";
+
+Deno.test("BST", async (t) => {
+  await t.step("insert", () => {
+    const root = new BST(10);
+    root.left = new BST(5);
+    root.left.left = new BST(2);
+    root.left.left.left = new BST(1);
+    root.left.right = new BST(5);
+    root.right = new BST(15);
+    root.right.left = new BST(13);
+    root.right.left.right = new BST(14);
+    root.right.right = new BST(22);
+
+    root.insert(12);
+
+    assertExists(root.right.left.left);
+    assertEquals(root.right.left.left.value, 12);
+  });
+
+  await t.step("contains", () => {
+    const root = new BST(10);
+    root.left = new BST(5);
+    root.left.left = new BST(2);
+    root.left.left.left = new BST(1);
+    root.left.right = new BST(5);
+    root.right = new BST(15);
+    root.right.left = new BST(13);
+    root.right.left.right = new BST(14);
+    root.right.right = new BST(22);
+
+    assertEquals(root.contains(100), false);
+    assertEquals(root.contains(13), true);
+  });
+
+  await t.step("remove", () => {
+    const root = new BST(10);
+    root.left = new BST(5);
+    root.left.left = new BST(2);
+    root.left.left.left = new BST(1);
+    root.left.right = new BST(7);
+    root.left.right.left = new BST(6);
+    root.left.right.right = new BST(8);
+    root.left.right.right.right = new BST(8);
+    root.right = new BST(15);
+    root.right.left = new BST(13);
+    root.right.left.left = new BST(12);
+    root.right.left.left.left = new BST(11);
+    root.right.left.right = new BST(14);
+    root.right.right = new BST(20);
+    root.right.right.right = new BST(25);
+    root.right.right.right.left = new BST(22);
+    root.right.right.right.left.left = new BST(21);
+    root.right.right.right.right = new BST(30);
+    root.right.right.right.right.left = new BST(29);
+    root.right.right.right.right.right = new BST(31);
+    root.right.right.right.right.right.right = new BST(32);
+
+    root.remove(1);
+    assertEquals(root.contains(1), false);
+    assertEquals(root.left.left.left, null);
+
+    root.remove(32);
+    assertEquals(root.contains(32), false);
+    assertEquals(root.right.right.right.right.right.right, null);
+
+    root.remove(20);
+    assertEquals(root.contains(20), false);
+    assertEquals(root.right.right.value, 25);
+
+    root.remove(12);
+    assertEquals(root.contains(12), false);
+    assertEquals(root.right.left.left.value, 11);
+
+    root.remove(25);
+    assertEquals(root.contains(25), false);
+    assertEquals(root.right.right.value, 29);
+    assertEquals(root.right.right.right.left, null);
+
+    root.remove(5);
+    assertEquals(root.contains(5), false);
+    assertEquals(root.left.value, 6);
+    assertEquals(root.left.right.left, null);
+
+    root.remove(29);
+    assertEquals(root.contains(29), false);
+    assertEquals(root.right.right.value, 30);
+
+    root.remove(6);
+    assertEquals(root.contains(6), false);
+    assertEquals(root.left.value, 7);
+
+    root.remove(10);
+    assertEquals(root.contains(10), false);
+    assertEquals(root.value, 11);
+    assertEquals(root.right.left.left, null);
+
+    root.remove(7);
+    assertEquals(root.contains(7), false);
+    assertEquals(root.left.value, 8);
+
+    root.remove(8);
+    assertEquals(root.left.value, 8);
+    assertEquals(root.left.right, null);
+
+    root.remove(8);
+    assertEquals(root.left.value, 2);
+    assertEquals(root.left.right, null);
+
+    root.remove(8);
+    assertEquals(root.contains(8), false);
+    assertEquals(root.left.value, 2);
+
+    root.remove(2);
+    assertEquals(root.contains(2), false);
+    assertEquals(root.left, null);
+
+    root.remove(11);
+    assertEquals(root.contains(11), false);
+    assertEquals(root.value, 15);
+    assertEquals(root.left.value, 13);
+    assertEquals(root.right.value, 30);
+
+    root.remove(13);
+    assertEquals(root.contains(13), false);
+    assertEquals(root.left.value, 14);
+    assertEquals(root.left.right, null);
+  });
+});
